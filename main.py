278380676a6e78c4345bf18efedc82b073b36615
Muhammad Ahmad Saeed <ahmadsaeed3220@gmail.com>
2024-08-05 03:08:10 +0500

    Removed Comments

--- main.py
+++ main.py
@@ -133,17 +133,13 @@ predicted = np.zeros(m)
 for i in range(m):
     predicted[i] = w * x_train[i] + b
 
-# Plot the linear fit
 plt.plot(x_train, predicted, c = "b")
 
-# Create a scatter plot of the data. 
 plt.scatter(x_train, y_train, marker='x', c='r') 
 
-# Set the title
 plt.title("Profits vs. Population per city")
-# Set the y-axis label
+
 plt.ylabel('Profit in $10,000')
-# Set the x-axis label
 plt.xlabel('Population of City in 10,000s')
 plt.show()
 
